8db452e7d6fb5d8bba3afb52b667662c4fc3350e
billyparrish <billy.parrish@ticketmaster.com>
2017-10-19 15:53:53 -0400

    added basic test harness

---
+++ presentation/src/test/java/com/tmsdurham/tridroidredux/ExampleUnitTest.kt
@@ -0,0 +1,40 @@
+package com.tmsdurham.tridroidredux
+
+import com.tmsdurham.DiscoverModel
+import data.location.ShortListCity
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    val cityMap: HashMap<ShortListCity, Boolean> = createCityMap()
+
+    fun createCityMap(): HashMap<ShortListCity, Boolean> {
+        val nyc  = ShortListCity(name = "New York, NY", radius = 12.65152529440462, latitude =40.72110286598638 , longitude =-73.89758759814248 )
+        val la = ShortListCity(name = "Los Angeles, CA", radius =46.16539413085751, latitude =36.210692, longitude =   -115.179365)
+
+        return mapOf(nyc to true, la to false) as HashMap<ShortListCity, Boolean>
+    }
+
+    val model: DiscoverModel = DiscoverModel(cityMap = mutableMapOf())
+
+
+    
+
+    @Test
+    fun itShouldInitializeState() {
+        val givenState = model.state
+        val givenCityMap = givenState.cityMap
+
+        model.dispatch(DiscoverModel.Action.Initialize(cityMap))
+
+        val statefulCityMap = model.state.cityMap
+        assertNotEquals(givenCityMap, statefulCityMap)
+    }
+
+}
